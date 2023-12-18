706252f045da23643ab12fba107ad4d8e28cac14
HudsinGod <mallelasindhuja@gmail.com>
2023-12-18 11:56:13 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -8,37 +8,27 @@ from langdetect import detect
 
 st.title("Visual to Auditory Transformation")
 
-# Set Tesseract OCR path
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
-# Language detection function
 def detect_language(text):
     detected_language = detect(text)
     return detected_language
 
-# Image to text conversion function
 def image_to_text(image, language='eng'):
     text = pytesseract.image_to_string(image, lang=language)
     return text
 
-# Text to speech conversion function
+
 def text_to_speech(text, output_language):
-     # Display loading spinner
-    with st.spinner("Converting to speech..."): 
-
-        # Generate speech
-        speech = gTTS(text, lang=output_language)
-    
-        # Save the speech as an MP3 file
+     
+    with st.spinner("Converting to speech..."):         
+        speech = gTTS(text, lang=output_language)    
         mp3_file_path = "output_audio.mp3"
-        speech.save(mp3_file_path)    
-    
-        # Display the audio file for playback
-        st.audio(mp3_file_path, format="audio/mp3")   
-
-        # Provide a link to download the audio file
+        speech.save(mp3_file_path)      
+        
+        st.audio(mp3_file_path, format="audio/mp3")        
         st.markdown(get_binary_file_downloader_html(mp3_file_path, 'Download Audio'), unsafe_allow_html=True)
-    # Stop the loading spinner
+   
     st.success("Speech conversion completed!")
     
     
@@ -56,9 +46,8 @@ def main_menu():
     choice = st.sidebar.selectbox("Select an option", ["Image to Text", "Image to Speech", "Text to Speech"])
 
     if choice == "Image to Text":
-        ocr_language = st.text_input("Enter OCR language code (e.g., 'hin' for Hindi, 'eng' for English, 'tel' for Telugu, 'tam' for Tamil):", 'eng')
-
-        # Image to text conversion
+        ocr_language = st.text_input("Enter OCR language code (e.g., 'hin' for Hindi, 'eng' for English, 'tel' for Telugu, 'tam' for Tamil, 'kan' for Kannada, 'mal' for Malayalam):", 'eng')
+        
         uploaded_image = st.file_uploader("Upload image", type=["jpg", "jpeg"])
         if uploaded_image:
             image = Image.open(uploaded_image)
@@ -67,21 +56,22 @@ def main_menu():
             st.text(extracted_text)
 
     elif choice == "Image to Speech":
-        # Image to speech conversion
-        ocr_language = st.text_input("Enter OCR language code (e.g., 'hin' for Hindi, 'eng' for English, 'tel' for Telugu, 'tam' for Tamil):", 'eng')
+        
+        ocr_language = st.text_input("Enter OCR language code (e.g., 'hin' for Hindi, 'eng' for English, 'tel' for Telugu, 'tam' for Tamil, 'kan' for Kannada, 'mal' for Malayalam):", 'eng')
 
         uploaded_image = st.file_uploader("Upload image", type=["jpg", "jpeg"])
         if uploaded_image:
             image = Image.open(uploaded_image)
             extracted_text = image_to_text(image, language=ocr_language)
             detected_language = detect_language(extracted_text)
-            # Display detected language
+            
             language_names = {
                 'en': 'English',
                 'hi': 'Hindi',
                 'te': 'Telugu',
-                'ta': 'Tamil'
-                # Add more languages as needed
+                'ta': 'Tamil',
+                'kn': 'Kannada',
+                'ml': 'Malayalam'              
             }
 
             if detected_language in language_names:
@@ -89,23 +79,23 @@ def main_menu():
             else:
                 st.text("Detected OCR language: Unknown")
 
-            output_language = st.text_input("Enter desired output language:", detected_language)
+            output_language = st.text_input("Enter desired output language:(e.g., 'hi' for Hindi, 'en' for English, 'te' for Telugu, 'ta' for Tamil, 'kn' for Kannada, 'ml' for Malayalam)", detected_language)
             if not output_language:
                 output_language = detected_language
             
             if st.button("Convert to Speech"):
-                if extracted_text.strip():  # Check if there's non-whitespace content
+                if extracted_text.strip(): 
                     text_to_speech(extracted_text, output_language)
                 else:
                     st.warning("No text extracted from the image. Please provide an image with readable text.")
 
     elif choice == "Text to Speech":
-        # Text to speech conversion
-        ocr_language = st.text_input("Enter OCR language code (e.g., 'hin' for Hindi, 'eng' for English, 'tel' for Telugu, 'tam' for Tamil):", 'eng')
+        
+        ocr_language = st.text_input("Enter OCR language code (e.g., 'hin' for Hindi, 'eng' for English, 'tel' for Telugu, 'tam' for Tamil, 'kan' for Kannada, 'mal' for Malayalam):", 'eng')
         input_text = st.text_area("Enter text to be converted:")
-        if input_text.strip():  # Check if there's non-whitespace content
+        if input_text.strip(): 
             detected_language = detect_language(input_text)
-            output_language = st.text_input("Enter desired output language:", detected_language)
+            output_language = st.text_input("Enter desired output language: (e.g., 'hi' for Hindi, 'en' for English, 'te' for Telugu, 'ta' for Tamil, 'kn' for Kannada, 'ml' for Malayalam)", detected_language)
             if not output_language:
                 output_language = detected_language            
             
